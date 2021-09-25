221dbed19bcc4f2d1019d7d6a19d33f64f127079
kybanez <72380279+kybanez@users.noreply.github.com>
2021-09-25 15:24:26 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    public delegate void priceCutEvent(Int32 pr);
+    
+    class Program
+    {
+        private const int K = 2;
+
+        private static Thread[] theater_threads = new Thread[K];
+
+        private static Theater[] theaters = new Theater[K];
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Theater Threading Simulator....");
+
+            for(int i = 0; i < K; ++i)
+            {
+                Theater theater = new Theater();
+                theaters[i] = theater;
+                theater_threads[i] = new Thread(theater.theaterFunc);
+                theater_threads[i].Name = "Theater: " + i;
+                theater_threads[i].Start();
+                while (!theater_threads[i].IsAlive) ;
+            }
+            ChickenFarm chickenFarm = new ChickenFarm();
+            Thread farmer = new Thread(new ThreadStart(chickenFarm.farmerFunc));
+            farmer.Start();
+            Retailers chickenStore = new Retailers();
+            ChickenFarm.priceCut += new priceCutEvent(chickenStore.chickenOnSale);
+            Thread[] retailers = new Thread[3];
+            for (int i = 0; i < 3; i++)
+            {
+                retailers[i] = new Thread(new ThreadStart(chickenStore.retailerFunc));
+                retailers[i].Name = (i + 1).ToString();
+                retailers[i].Start();
+            }
+        }
+    }
+}
---
+++ Theater.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    class Theater
+    {
+
+        private const Int32 max_priceCut = 20;
+        private int priceCut_cnt = 1;
+
+        private double new_Price = 0.0;
+        private double prev_Price = 0.0;
+
+        private static Random rnd = new Random();
+        private PriceCutEvent pricecutevent;
+
+        public delegate void PriceCutEvent(PriceCutEventHandler p);
+
+        private ArrayList orderThreads = new ArrayList();
+
+        public double getPrice()
+        {
+            return new_Price;
+        }
+
+        //Stop after t-many price cuts
+        public void theaterFunc()
+        {
+           while (priceCut_cnt <= max_priceCut)
+            {
+                Set_ticketPrice();
+
+                if (new_Price < prev_Price) ;
+                {
+                    PriceCutEvent();
+                }
+
+                ProcessOrder(GetOrder(), new_Price);
+            }
+
+           foreach (Thread item in processingThreads)
+            {
+                while (item.IsAlive) ;
+            }
+        }
+
+        //Using pricing model to set dynamic pricing
+
+
+
+        //Emmiting event to ticketbroker if price change (new price < oldprice)
+
+
+        //Receiving order from MultiCellBuffer
+
+
+        //Ccreate order Processing thread
+
+        //Method started by main
+
+        //Defines a price cut event to call event handlers in broker
+        //For each order, spawn an order processing thread
+        
+    }
+
+}
