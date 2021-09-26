e4d34308d2b133b0d617e81b8047a1de4e072162
mechrodancer <stickman331@gmail.com>
2021-09-25 17:51:18 -0700

    Update OrderProcessing.cs

--- OrderProcessing.cs
+++ OrderProcessing.cs
@@ -6,7 +6,46 @@ using System.Threading.Tasks;
 
 namespace Project2
 {
-    class Class1
+    public class OrderProcessing
     {
+      //uses OrerClass.cs
+      private OrderClass OrderObject;
+      private double TicketPrice;
+
+
+      public OrderProcessing(OrderClass Torder, double price)
+     {
+         OrderObject = Torder;
+         TicketPrice = price;
+     }
+
+     private bool CCard(int cnumb)
+      {
+        if ( cnumb >= 5000 && ncumb <= 7000)
+          return;
+      }
+      private double Tax = .12;
+      private double LocationChange = .08;
+
+      private double ChargeStuff(int tickets, double unitPrice)
+       {
+           double total = ( tickets *unitPrice) + Tax + LocationChange);
+           return TaxRate * total + total;
+       }
+
+       public void POrder(){
+//CardNo variable is made in OderClass.cs
+         if (CheckCreditCard(OrderObject.CardNo))
+                 {
+                     double finalCharge = ChargeStuff(OrderObject.Amount, TicketPrice);
+                     cout << ("Order from \n\tOrder Cost: " + OrderObject.finalCharge);
+                     //still needs to send confermation to Ticket broker
+                 }
+                 else
+                 {
+                     cout << ("Order from " +OrderObject.Id +" has invalid credit card number " + OrderObject.CardNo);
+                 }
+       }
     }
+
 }
